5ca9a37d830283a270837b846682f2ec477f2674
ramod <raihaan.amod@gmail.com>
2025-08-14 02:02:40 +1000

    blog tweaks

--- Components/GridPostsArchive/functions.php
+++ Components/GridPostsArchive/functions.php
@@ -32,12 +32,14 @@ add_filter('Flynt/addComponentData?name=GridPostsArchive', function (array $data
         // Add item for all posts
         array_unshift($data['terms'], [
             'link' => get_post_type_archive_link($postType),
-            'title' => $data['labels']['allPosts'],
+            'title' => 'All',
             'isActive' => is_home() || is_post_type_archive($postType),
         ]);
     }
 
-    if (is_home()) {
+    if (is_search()) {
+        $data['title'] = __('Search Results', 'flynt');
+    } else if (is_home()) {
         $data['isHome'] = true;
         $data['title'] = $queriedObject->post_title ?? get_bloginfo('name');
     } else {
@@ -49,142 +51,3 @@ add_filter('Flynt/addComponentData?name=GridPostsArchive', function (array $data
 });
 
 
-// Options::addGlobal('GridPostsArchive', [
-//     [
-//         'label' => __('Load More Button?', 'flynt'),
-//         'name' => 'loadMore',
-//         'type' => 'true_false',
-//         'default_value' => 1,
-//         'ui' => 1
-//     ],
-// ]);
-
-Options::addTranslatable('GridPostsArchive', [
-    [
-        'label' => __('Content', 'flynt'),
-        'name' => 'contentTab',
-        'type' => 'tab',
-        'placement' => 'top',
-        'endpoint' => 0,
-    ],
-    [
-        'label' => __('Title', 'flynt'),
-        'instructions' => __('Want to add a headline? And a paragraph? Go ahead! Or just leave it empty and nothing will be shown.', 'flynt'),
-        'name' => 'preContentHtml',
-        'type' => 'wysiwyg',
-        'tabs' => 'visual,text',
-        'media_upload' => 0,
-        'delay' => 0,
-    ],
-    [
-        'label' => __('Labels', 'flynt'),
-        'name' => 'labelsTab',
-        'type' => 'tab',
-        'placement' => 'top',
-        'endpoint' => 0
-    ],
-    [
-        'label' => '',
-        'name' => 'labels',
-        'type' => 'group',
-        'sub_fields' => [
-            [
-                'label' => __('Filter by', 'flynt'),
-                'name' => 'filterBy',
-                'type' => 'text',
-                'default_value' => __('Filter by', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('Previous', 'flynt'),
-                'name' => 'previous',
-                'type' => 'text',
-                'default_value' => __('Prev', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('Next', 'flynt'),
-                'name' => 'next',
-                'type' => 'text',
-                'default_value' => __('Next', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('Load More', 'flynt'),
-                'name' => 'loadMore',
-                'type' => 'text',
-                'default_value' => __('Load More', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('No Posts Found Text', 'flynt'),
-                'name' => 'noPostsFound',
-                'type' => 'text',
-                'default_value' => __('No post found.', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('All Posts', 'flynt'),
-                'name' => 'allPosts',
-                'type' => 'text',
-                'default_value' => __('All', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ],
-            [
-                'label' => __('Reading Time - (20) min read', 'flynt'),
-                'instructions' => __('%d is placeholder for number of minutes', 'flynt'),
-                'name' => 'readingTime',
-                'type' => 'text',
-                'default_value' => __('%d min read', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => 50
-                ],
-            ],
-            [
-                'label' => __('Read More', 'flynt'),
-                'name' => 'readMore',
-                'type' => 'text',
-                'default_value' => __('Read More', 'flynt'),
-                'required' => 1,
-                'wrapper' => [
-                    'width' => '50',
-                ],
-            ]
-        ],
-    ],
-    [
-        'label' => __('Options', 'flynt'),
-        'name' => 'optionsTab',
-        'type' => 'tab',
-        'placement' => 'top',
-        'endpoint' => 0
-    ],
-    [
-        'label' => '',
-        'name' => 'options',
-        'type' => 'group',
-        'layout' => 'row',
-        'sub_fields' => [
-            FieldVariables\getTheme()
-        ]
-    ],
-]);
