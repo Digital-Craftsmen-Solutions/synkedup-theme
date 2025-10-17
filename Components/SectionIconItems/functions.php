5bb8bf6d26c6a04ccc79c252c15dc9a37a19b126
ramod <raihaan.amod@gmail.com>
2025-10-17 14:09:45 +1000

    fields for section icon items

--- Components/SectionIconItems/functions.php
+++ Components/SectionIconItems/functions.php
@@ -7,7 +7,7 @@ use Flynt\FieldVariables;
 
 add_filter('Flynt/addComponentData?name=SectionIconItems', function (array $data): array {
     $model = [
-
+        'items' => $data['items'] ?: [],
         'options' => $data['options'] ?: []
     ];
 
@@ -28,6 +28,81 @@ function getACFLayout(): array
                 'endpoint' => 0,
             ],
             [
+                'label' => __('Items', 'flynt'),
+                'name' => 'items',
+                'type' => 'repeater',
+                'layout' => 'block',
+                'min' => 0,
+                'max' => 3,
+                'button_label' => __('Add Item', 'flynt'),
+                'sub_fields' => [
+                    [
+                        'label' => __('Type', 'flynt'),
+                        'name' => 'type',
+                        'type' => 'button_group',
+                        'choices' => [
+                            'icon' => __('Icon', 'flynt'),
+                            'image' => __('Image', 'flynt'),
+                            'figure' => __('Figure', 'flynt'),
+                        ],
+                        'default_value' => 'icon',
+                        'layout' => 'horizontal',
+                        'wrapper' => ['width' => 20],
+                    ],
+                    array_merge(
+                        FieldVariables\getIcon(),
+                        [
+                            'conditional_logic' => [
+                                [
+                                    [
+                                        'fieldPath' => 'type',
+                                        'operator' => '==',
+                                        'value' => 'icon',
+                                    ],
+                                ],
+                            ],
+                        ]
+                    ),
+                    [
+                        'label' => __('Image', 'flynt'),
+                        'name' => 'image',
+                        'type' => 'image',
+                        'return_format' => 'array',
+                        'conditional_logic' => [
+                            [
+                                [
+                                    'fieldPath' => 'type',
+                                    'operator' => '==',
+                                    'value' => 'image',
+                                ],
+                            ],
+                        ],
+                        'wrapper' => ['width' => 20],
+                    ],
+                    [
+                        'label' => __('Figure', 'flynt'),
+                        'name' => 'figure',
+                        'type' => 'text',
+                        'conditional_logic' => [
+                            [
+                                [
+                                    'fieldPath' => 'type',
+                                    'operator' => '==',
+                                    'value' => 'figure',
+                                ],
+                            ],
+                        ],
+                        'wrapper' => ['width' => 20],
+                    ],
+                    [
+                        'label' => __('Description', 'flynt'),
+                        'name' => 'description',
+                        'type' => 'text',
+                        'wrapper' => ['width' => 30],
+                    ],
+                ]
+            ],
+            [
                 'label' => __('Options', 'flynt'),
                 'name' => 'optionsTab',
                 'type' => 'tab',
@@ -40,6 +115,17 @@ function getACFLayout(): array
                 'type' => 'group',
                 'layout' => 'row',
                 'sub_fields' => [
+                    [
+                        'label' => __('Align', 'flynt'),
+                        'name' => 'align',
+                        'type' => 'button_group',
+                        'choices' => [
+                            'left' => __('Left', 'flynt'),
+                            'center' => __('Center', 'flynt'),
+                        ],
+                        'default_value' => 'center',
+                        'layout' => 'horizontal',
+                    ],
                     FieldVariables\getTheme()
                 ],
             ],
